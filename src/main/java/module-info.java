5b4e66cab83b375588fb83319ee1a848a014cea1
Ethank1354 <kiers.ethan@gmail.com>
2025-03-16 16:00:45 -0400

    add new method

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,5 @@
 module com.ethankiers.databasemanagement.databasemanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires org.apache.poi.ooxml;
     requires org.apache.logging.log4j;
     requires java.sql;
